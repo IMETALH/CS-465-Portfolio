f7aa073b8c2673c5d5d6a871c051c659aa7d9a6d
kennedy <kennedyuzoho@outlook.com>
2024-08-31 00:58:42 -0400

    convert travel template to dynamic JSON using hbs

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,7 @@
 // require('dotenv').config();
+
 const fs = require('fs');
+
 const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const trips = JSON.parse(fs.readFileSync('app_server/data/trips.json', 'utf8'));
 // const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
@@ -10,6 +12,7 @@ const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
     res.render('travel', { title: pageTitle, trips });
 }; */
+
 const travel = (req, res) => {
     pageTitle = packageJson.description + ' | Travel';
     res.render('travel', { title: pageTitle, trips });
