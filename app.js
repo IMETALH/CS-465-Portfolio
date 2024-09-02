4198a4e1a70b7bb935204d5ae4802f95589f7f27
kennedy <kennedyuzoho@outlook.com>
2024-09-02 10:26:32 -0400

    update project files

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-// include reference to handlebars code
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+// reference to handlebars code
 const hbs = require('hbs');
-// set up database connection
+// set up database connection path
 require('./app_api/models/db');
 
-// set up routers
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
-var aboutRouter = require('./app_server/routes/about');
-var contactRouter = require('./app_server/routes/contact');
-var mealsRouter = require('./app_server/routes/meals');
-var newsRouter = require('./app_server/routes/news');
-var roomsRouter = require('./app_server/routes/rooms');
+// require routers
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
+const aboutRouter = require('./app_server/routes/about');
+const contactRouter = require('./app_server/routes/contact');
+const mealsRouter = require('./app_server/routes/meals');
+const newsRouter = require('./app_server/routes/news');
+const roomsRouter = require('./app_server/routes/rooms');
 const apiRouter = require('./app_api/routes/index');
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'hbs');
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,23 +34,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// allow CORS
+// Allow CORS
 app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requesteed-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header(
+    'Access-Control-Allow-Origin',
+    'http://localhost:4200'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE'
+  );
   next();
 });
 
-// use routers for specific URLs
+// use routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/travel', travelRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
 app.use('/meals', mealsRouter);
 app.use('/news', newsRouter);
 app.use('/rooms', roomsRouter);
+app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
@@ -69,7 +78,7 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-  // Custom Handlebars helper to check if the current page matches the active page
+// Custom Handlebars helper to check if the current page matches the active page
 hbs.registerHelper('activePage', function(page, options) {
   const currentPage = options.data.root.activePage;
   return currentPage === page ? 'selected' : '';
