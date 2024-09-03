f180edf3625c6e0c3d68396ab682edbe33441e6a
kennedy <kennedyuzoho@outlook.com>
2024-09-02 21:38:45 -0400

    update project files

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-// reference to handlebars code
+// include reference to handlebars
 const hbs = require('hbs');
-// set up database connection path
-require('./app_api/models/db');
+// set up database connection
+require('./app_api/database/db');
 
-// require routers
+// set up routers
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
@@ -33,23 +33,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Allow CORS
-app.use('/api', (req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'http://localhost:4200'
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE'
-  );
-  next();
-});
 
 // use routes
 app.use('/', indexRouter);
